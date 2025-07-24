3f7f963e4f30ec3bdf2da5cb55a30c3ed90df1d7
John VonAchen <jackvonachen@gmail.com>
2025-07-24 11:35:27 -0500

    movement updated

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+	[SerializeField] private float moveSpeed = 7f;
 	private void Update() {
 		Vector2 inputVector = new Vector2(0, 0);
 
@@ -18,7 +20,9 @@ public class Player : MonoBehaviour
 			inputVector.x = +1;
 		} 
 		inputVector = inputVector.normalized;
-		transform.position += (Vector3)inputVector; 
-		Debug.Log(inputVector);
+
+		Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+		transform.position += moveDir * moveSpeed * Time.deltaTime;
+		Debug.Log(Time.deltaTime);
 	}
 }
