4a44a30cecb1f5d12e93c64875e6a712321e6fbb
Jack VonAchen <jackvonachen@gmail.com>
2025-07-30 10:16:12 -0500

    added diagonal movement to collision detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,26 +9,67 @@ public class Player : MonoBehaviour
 	[SerializeField] private GameInput gameInput;
 
 	private bool isWalking;
-	private void Update() {
+	private void Update()
+	{
 		Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+		Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
 		float moveDistance = moveSpeed * Time.deltaTime;
 		float playerRadius = .7f;
 		float playerHeight = 2f;
-		bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
 
-		if (canMove) {
-			transform.position += moveDir * moveDistance;
-		}
+        if (!canMove)
+        {
+            // Cannot move towards moveDir
 
-		isWalking = moveDir != Vector3.zero;
-		float rotateSpeed = 10f;
-		transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-	}
+            // Attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
-	public bool IsWalking() {
+            if (canMove)
+            {
+                // can move only on the x axis
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // Cannot move only on the x axis
+
+                // Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    // can move only on the z axis
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    // Cannot move in any direction
+                    moveDir = Vector3.zero;
+                }
+            }
+        }
+
+        if (canMove && moveDir != Vector3.zero)
+        {
+            transform.position += moveDir * moveDistance;
+
+            isWalking = true;
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        }
+        else
+        {
+            isWalking = false;
+        }
+
+    }
+
+    public bool IsWalking() {
 		return isWalking;
 	}
 
