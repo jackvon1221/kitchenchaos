cce562d1c45069ad23df720136d6937c2104aae8
Jack VonAchen <jackvonachen@gmail.com>
2025-07-30 10:18:18 -0500

    normalized movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,7 +25,7 @@ public class Player : MonoBehaviour
             // Cannot move towards moveDir
 
             // Attempt only X movement
-            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
             if (canMove)
@@ -38,7 +38,7 @@ public class Player : MonoBehaviour
                 // Cannot move only on the x axis
 
                 // Attempt only Z movement
-                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
                 canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
 
                 if (canMove)
